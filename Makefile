bfe32f478574481f3d4c5b63d959a1233d3fcc1a
Stephen Adams <tsadams@gmail.com>
2021-07-19 09:45:09 -0400

    [task] organize and add needed files
    Just doing some basic setup here
    
    Signed-off-by: Stephen Adams <tsadams@gmail.com>

--- Makefile
+++ Makefile
@@ -1,12 +1,12 @@
 build-api:
 	
-	go build -o payload-tracker-api cmd/payload-tracker-api/main.go
+	go build -o pt-api cmd/payload-tracker-api/main.go
 
 build-consumer:
 
-	go build -o payload-tracker-consumer cmd/payload-tracker-consumer/main.go
+	go build -o pt-consumer cmd/payload-tracker-consumer/main.go
 
 build-all:
 
-	go build -o payload-tracker-api cmd/payload-tracker-api/main.go
-	go build -o payload-tracker-consumer cmd/payload-tracker-consumer/main.go
+	go build -o pt-api cmd/payload-tracker-api/main.go
+	go build -o pt-consumer cmd/payload-tracker-consumer/main.go
---
+++ cmd/payload-tracker-api/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+
+func lubdub(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("lubdub"))
+}
+
+
+func main() {
+
+
+	r := chi.NewRouter()
+	mr := chi.NewRouter()
+	sub := chi.NewRouter()
+
+	// Mount the root of the api router on /api/v1
+	r.Mount("/api/v1/", sub)
+	r.Get("/", lubdub)
+
+	// Mount the metrics handler on /metrics
+	mr.Get("/", lubdub)
+	mr.Handle("/metrics", promhttp.Handler())
+
+	sub.Get("/", lubdub)
+
+	srv := http.Server{
+		Addr:	":8080",
+		Handler: r,
+	}
+
+	msrv := http.Server{
+		Addr:	":8081",
+		Handler: mr,
+	}
+
+	go func() {
+
+		if err := msrv.ListenAndServe(); err != nil {
+			panic(err)
+		}
+	}()
+
+	if err := srv.ListenAndServe(); err != nil {
+		panic(err)
+	}
+}
