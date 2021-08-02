3a81bddf6c68953358db47a12bf802973d393bbf
Tahmid Efaz <tefaz@redhat.com>
2021-08-02 16:59:32 -0400

    add autoincrement function for ids (#7)

--- models/models.go
+++ models/models.go
@@ -16,7 +16,7 @@ type PayloadStatuses struct {
 }
 
 type Payloads struct {
-	Id uint `gorm:"primaryKey;not null"`
+	Id uint `gorm:"primaryKey;not null;autoIncrement"`
 	RequestId string `gorm:"not null;type:varchar"`
 	Account string `gorm:"type:varchar"`
 	InventoryId string `gorm:"type:varchar"`
@@ -25,16 +25,16 @@ type Payloads struct {
 }
 
 type Services struct {
-	Id int32 `gorm:"primaryKey;not null"`
+	Id int32 `gorm:"primaryKey;not null;autoIncrement"`
 	Name string `gorm:"not null;type:varchar"`
 }
 
 type Sources struct {
-	Id int32 `gorm:"primaryKey;not null"`
+	Id int32 `gorm:"primaryKey;not null;autoIncrement"`
 	Name string `gorm:"not null;type:varchar"`
 }
 
 type Statuses struct {
-	Id int32 `gorm:"primaryKey;not null"`
+	Id int32 `gorm:"primaryKey;not null;autoIncrement"`
 	Name string `gorm:"not null;type:varchar"`
 }
