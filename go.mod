7a877bcf02abb1c7416c4bc96fc3c065e594783d
Stephen Adams <tsadams@gmail.com>
2021-08-02 15:23:57 -0400

    [feat] Add cloudwatch logging config
    Signed-off-by: Stephen Adams <tsadams@gmail.com>

--- go.mod
+++ go.mod
@@ -6,5 +6,6 @@ require (
 	github.com/go-chi/chi/v5 v5.0.3
 	github.com/prometheus/client_golang v1.11.0
 	github.com/redhatinsights/app-common-go v1.5.1
+	github.com/sirupsen/logrus v1.6.0
 	github.com/spf13/viper v1.8.1
 )
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"os"
 	"strings"
 
 	"github.com/spf13/viper"
@@ -14,6 +15,7 @@ type TrackerConfig struct {
 	PublicPort  string
 	MetricsPort string
 	KafkaConfig KafkaCfg
+	CloudwatchConfig CloudwatchCfg
 }
 
 type KafkaCfg struct {
@@ -33,6 +35,13 @@ type KafkaCfg struct {
 	Protocol                   string
 }
 
+type CloudwatchCfg struct {
+	CWLogGroup  string
+	CWRegion    string
+	CWAccessKey string
+	CWSecretKey string
+}
+
 // Get sets each config option with its defaults
 func Get() *TrackerConfig {
 	options := viper.New()
@@ -46,15 +55,33 @@ func Get() *TrackerConfig {
 	options.SetDefault("kafka.retry.backoff.ms", 100)
 
 	if clowder.IsClowderEnabled() {
+		cfg := clowder.LoadedConfig
+
+		// kafka
 		options.SetDefault("kafka.brokers", clowder.KafkaServers)
 		options.SetDefault("topic.payload.status", clowder.KafkaTopics["platform.payload-status"].Name)
-		options.SetDefault("publicPort", clowder.LoadedConfig.PublicPort)
-		options.SetDefault("metricsPort", clowder.LoadedConfig.MetricsPort)
+		// ports
+		options.SetDefault("publicPort", cfg.PublicPort)
+		options.SetDefault("metricsPort", cfg.MetricsPort)
+		// cloudwatch
+		options.SetDefault("logGroup", cfg.Logging.Cloudwatch.LogGroup)
+		options.SetDefault("cwRegion", cfg.Logging.Cloudwatch.Region)
+		options.SetDefault("cwAccessKey", cfg.Logging.Cloudwatch.AccessKeyId)
+		options.SetDefault("cwSecretKey", cfg.Logging.Cloudwatch.SecretAccessKey)
+
 	} else {
+		
+		// kafka
 		options.SetDefault("kafka.brokers", []string{"localhost:29092"})
 		options.SetDefault("topic.payload.status", "platform.payload-status")
+		// ports
 		options.SetDefault("publicPort", "8080")
 		options.SetDefault("metricsPort", "8081")
+		// cloudwatch
+		options.SetDefault("logGroup", "platform-dev")
+		options.SetDefault("cwRegion", "us-east-1")
+		options.SetDefault("cwAccessKey", os.Getenv("CW_AWS_ACCESS_KEY_ID"))
+		options.SetDefault("cwSecretKey", os.Getenv("CW_AWS_SECRET_ACCESS_KEY"))
 	}
 
 	options.AutomaticEnv()
@@ -73,6 +100,12 @@ func Get() *TrackerConfig {
 			KafkaRetryBackoffMs:        options.GetInt("kafka.retry.backoff.ms"),
 			KafkaBrokers:               options.GetStringSlice("kafka.brokers"),
 			KafkaTopic:                 options.GetString("topic.payload.status"),
+		},
+		CloudwatchConfig: CloudwatchCfg{
+			CWLogGroup:  options.GetString("logGroup"),
+			CWRegion:    options.GetString("cwRegion"),
+			CWAccessKey: options.GetString("cwAccessKey"),
+			CWSecretKey: options.GetString("cwSecretKey"),
 		},
 	}
 
