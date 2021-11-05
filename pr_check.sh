546d2cccf09b31ddb847c0879b66010b56358fa6
Alec Cohan <alcohan@redhat.com>
2021-11-05 16:00:45 -0400

    Add artifacts section to PR_CHECK.sh

--- pr_check.sh
+++ pr_check.sh
@@ -22,3 +22,10 @@ source $CICD_ROOT/build.sh
 # source $APP_ROOT/unit_test.sh
 source $CICD_ROOT/deploy_ephemeral_env.sh
 # source $CICD_ROOT/cji_smoke_test.sh
+
+mkdir -p $WORKSPACE/artifacts
+cat << EOF > $WORKSPACE/artifacts/junit-dummy.xml
+<testsuite tests="1">
+    <testcase classname="dummy" name="dummytest"/>
+</testsuite>
+EOF
