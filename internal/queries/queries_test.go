d7cd0d0fcd683b8cea30ff7b9b375109d3e2739b
Tahmid Efaz <tefaz@redhat.com>
2022-05-11 13:30:15 -0400

    Using database to run tests in github actions (#91)
    * using database to run tests in github actions
    
    * adding db migrate command for the github actions job
    
    * updating README

---
+++ internal/queries/queries_test.go
@@ -0,0 +1,37 @@
+package queries
+
+import (
+	models "github.com/redhatinsights/payload-tracker-go/internal/models/db"
+	"github.com/redhatinsights/payload-tracker-go/internal/utils/test"
+
+	"github.com/google/uuid"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func getUUID() string {
+	return uuid.New().String()
+}
+
+
+var _ = Describe("Queries", func() {
+	db := test.WithDatabase()
+
+	It("Retrieves request id payload", func() {
+		requestId := getUUID()
+		payload := models.Payloads{
+			RequestId: requestId,
+			Account: "1234",
+			InventoryId: getUUID(),
+			SystemId: getUUID(),
+		}
+		Expect(db().Create(&payload).Error).ToNot(HaveOccurred())
+
+		payload, err := GetPayloadByRequestId(db(), requestId)
+
+		Expect(err).ToNot(HaveOccurred())
+		
+		Expect(payload.RequestId).To(Equal(requestId))
+		Expect(payload.Account).To(Equal("1234"))
+	})
+})
