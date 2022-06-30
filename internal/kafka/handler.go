24bf3185b311242def0500d67ca56c918e9d109b
Stephen Adams <stephen.adams@redhat.com>
2022-06-30 15:44:06 -0400

    [fix] Accept only request IDs of a valid length
    We currently are accepting shorter request_ids which is resulting in a
    lot of test payloads with a -1 ID getting through. We don't want that
    
    Signed-off-by: Stephen Adams <stephen.adams@redhat.com>

--- internal/kafka/handler.go
+++ internal/kafka/handler.go
@@ -42,7 +42,7 @@ func (this *handler) onMessage(ctx context.Context, msg *kafka.Message, cfg *con
 
 	// Validate RequestID
 	if cfg.RequestConfig.ValidateRequestIDLength != 0 {
-		if len(payloadStatus.RequestID) > cfg.RequestConfig.ValidateRequestIDLength {
+		if len(payloadStatus.RequestID) != cfg.RequestConfig.ValidateRequestIDLength {
 			return
 		}
 	}
