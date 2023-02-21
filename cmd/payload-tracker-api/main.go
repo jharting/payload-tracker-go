8ce42d3fab1a0733ca245c67da04c2b9c52a0a53
Stephen Adams <stephen.adams@redhat.com>
2023-02-21 11:52:14 -0500

    Clean up unnecessary endpoints
    Signed-off-by: Stephen Adams <stephen.adams@redhat.com>

--- cmd/payload-tracker-api/main.go
+++ cmd/payload-tracker-api/main.go
@@ -68,7 +68,6 @@ func main() {
 	sub.With(endpoints.ResponseMetricsMiddleware).Get("/payloads/{request_id}/kibanaLink", endpoints.PayloadKibanaLink)
 	sub.With(endpoints.ResponseMetricsMiddleware).Get("/roles/archiveLink", endpoints.RolesArchiveLink)
 	sub.With(endpoints.ResponseMetricsMiddleware).Get("/statuses", endpoints.Statuses)
-	sub.With(endpoints.ResponseMetricsMiddleware).Get("/dummy", endpoints.Dummy)
 
 	srv := http.Server{
 		Addr:    ":" + cfg.PublicPort,
