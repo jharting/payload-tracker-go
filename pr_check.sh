3f6d1a118ca788dd3a9347139d7198eafe2f3ab7
dehort <dehort@redhat.com>
2024-09-12 15:42:41 -0500

    Adjust component name (#209)

--- pr_check.sh
+++ pr_check.sh
@@ -4,7 +4,7 @@
 # Options that must be configured by app owner
 # --------------------------------------------
 APP_NAME="payload-tracker"  # name of app-sre "application" folder this component lives in
-COMPONENT_NAME="payload-tracker-go"  # name of app-sre "resourceTemplate" in deploy.yaml for this component
+COMPONENT_NAME="payload-tracker"  # name of app-sre "resourceTemplate" in deploy.yaml for this component
 IMAGE="quay.io/cloudservices/payload-tracker-go"  
 
 # ADD BACK IN WHEN PAYLOAD-TRACKER-GO HAS SMOKE TESTS
